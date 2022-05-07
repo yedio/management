68afae7ff444777baaadc3f47d43485f4f0c6f5a
yedio <yediobjet@gmail.com>
2022-05-07 10:41:39 +0900

    Add: client 폴더구조 변경 및 server 개발환경구축

---
+++ server.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
